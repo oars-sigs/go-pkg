6f6aa26d9e944f4f250936895b514334c9c3244d
hashwing <1144205233@qq.com>
2022-04-08 14:19:52 +0800

    add static gzip

--- server/static/dist.go
+++ server/static/dist.go
@@ -1,8 +1,10 @@
 package static
 
 import (
+	"compress/gzip"
 	"embed"
 	"fmt"
+	"io/ioutil"
 	"mime"
 	"strings"
 
@@ -14,14 +16,34 @@ func WrapHandler(dir, contextPath string, static embed.FS) func(*gin.Context) {
 		path := g.Request.URL.Path
 		s := strings.Split(path, ".")
 		contentType := mime.TypeByExtension(fmt.Sprintf(".%s", s[len(s)-1]))
-		data, err := static.ReadFile(dir + path)
+		fs, err := static.Open(dir + path)
 		if err != nil {
-			data, err = static.ReadFile(dir + contextPath + "/index.html")
+			fs, err = static.Open(dir + contextPath + "/index.html")
 			contentType = mime.TypeByExtension(".html")
 			if err != nil {
 				g.AbortWithStatus(404)
 			}
 		}
+		if !strings.Contains(g.GetHeader("Accept-Encoding"), "gzip") {
+			data, err := ioutil.ReadAll(fs)
+			if err != nil {
+				g.AbortWithStatus(500)
+				return
+			}
+			g.Data(200, contentType, data)
+			return
+		}
+		gr, err := gzip.NewReader(fs)
+		if err != nil {
+			g.AbortWithStatus(500)
+			return
+		}
+		data, err := ioutil.ReadAll(gr)
+		if err != nil {
+			g.AbortWithStatus(500)
+			return
+		}
+		g.Header("Content-Encoding", "gzip")
 		g.Data(200, contentType, data)
 	}
 }
