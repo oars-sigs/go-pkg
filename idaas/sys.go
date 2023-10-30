7440aad3b70f59ca866025e6551e7670b7a24125
hashwing <1144205233@qq.com>
2023-10-30 10:34:04 +0800

    update sys

---
+++ idaas/sys.go
@@ -0,0 +1,25 @@
+package idaas
+
+import (
+	"net/http"
+
+	"pkg.oars.vip/go-pkg/req"
+	"pkg.oars.vip/go-pkg/server/base"
+)
+
+type SysResp struct {
+	*base.DataResponse
+	Data map[string]interface{} `json:"data"`
+}
+
+func (c *Client) Sys(param string) (map[string]interface{}, error) {
+	var res SysResp
+	err := req.ReqJSON(http.MethodGet, c.getUrl("/idaas-app/sys?param="+param), nil, &res, nil)
+	if err != nil {
+		return nil, err
+	}
+	if res.Error.Error() != nil {
+		return nil, err
+	}
+	return res.Data, err
+}
