f2c1a3851612c2f7a20ad53217f0787f6cade9b6
hashwing <1144205233@qq.com>
2024-12-10 14:24:19 +0800

    add import

---
+++ server/crud/import.go
@@ -0,0 +1,103 @@
+package crud
+
+import (
+	"encoding/json"
+	"os"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/xuri/excelize/v2"
+)
+
+type ImportOption struct {
+	Sheet     string         `json:"sheet"`
+	OffsetRow int            `json:"offsetRow"`
+	Column    []ImportColumn `json:"column"`
+}
+
+type ImportColumn struct {
+	Label    string          `json:"label"`
+	Prop     string          `json:"prop"`
+	Type     string          `json:"type"`
+	TableDic *ImportTableDic `json:"tableDic"`
+	DataDic  []ImportDataDic `json:"dataDic"`
+}
+
+type ImportTableDic struct {
+	Table string `json:"table"`
+	Label string `json:"label"`
+	Value string `json:"value"`
+}
+
+type ImportDataDic struct {
+	Label string `json:"label"`
+	Value string `json:"value"`
+}
+
+func ParseImport(g *gin.Context, opt *ImportOption, res interface{}) error {
+	fs, err := g.FormFile("file")
+	if err != nil {
+		return err
+	}
+	path := "/tmp/" + uuid.NewString()
+	err = g.SaveUploadedFile(fs, path)
+	if err != nil {
+		return err
+	}
+	defer os.RemoveAll(path)
+	f, err := excelize.OpenFile(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	rows, err := f.GetRows(opt.Sheet)
+	if err != nil {
+		return err
+	}
+
+	var hs = make(map[int]ImportColumn)
+	for i, row := range rows {
+		if i == opt.OffsetRow {
+			for j, r := range row {
+				for n, col := range opt.Column {
+					if col.Label == r {
+						hs[j] = opt.Column[n]
+					}
+				}
+			}
+			break
+		}
+	}
+	var data []map[string]interface{}
+	for i, row := range rows {
+		if i <= opt.OffsetRow {
+			continue
+		}
+		item := make(map[string]interface{})
+		for j, r := range row {
+			if h, ok := hs[j]; ok {
+				if h.Type == "int" {
+					s, err := strconv.Atoi(r)
+					if err != nil {
+						return err
+					}
+					item[h.Prop] = s
+				} else {
+					item[h.Prop] = r
+				}
+			}
+		}
+		data = append(data, item)
+	}
+	d, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(d, &res)
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
